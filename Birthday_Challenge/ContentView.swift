2fb0817f0a2fde7e8201200945713d5247f9897f
BlakeT <BlakeTswift@gmail.com>
2025-09-19 14:49:53 -0600

    A simple app that can help celebrate birthdays and some other fuetures on top of that

--- Birthday_Challenge/ContentView.swift
+++ Birthday_Challenge/ContentView.swift
@@ -8,16 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name: String = ""
+    @State private var selectedDate: Date = Date()
+    @State private var custom: Color = .blue
+    @State private var mode = false
+    @State private var fontSize = 20.0
+    @State private var pressed: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Rectangle()
+                .fill(BackgroundMode())
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea(edges: .all)
+
+            VStack {
+                Toggle("Dark mode? \(mode ? "(Off)" : "(On)")",isOn: $mode)
+                
+                Text("Text Size:")
+                    .font(.title2)
+                Slider(value: $fontSize, in: 10...35) {
+                    //accessabilty
+                }minimumValueLabel: {
+                    Image(systemName: "minus")
+                }maximumValueLabel: {
+                    Image(systemName: "plus")
+                }
+                Spacer(minLength: 20)
+                VStack {
+                    TextField("Who's Birthday is is?:",text: $name)
+                    DatePicker("Whats the date today?:",selection: $selectedDate)
+                }
+                .padding()
+                .background()
+                    
+                
+                Spacer()
+                
+                ColorPicker("Pick A font Color:", selection: $custom)
+                
+                Spacer()
+                
+                HStack {
+                    Button("Press Me") {
+                         pressed.toggle()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .foregroundColor(.yellow)
+                    .fontWeight(.black)
+                    
+                    Text("Your Date: \(selectedDate.formatted())"
+                    )
+                    Text("Happy Birthday \(name)!")
+                        .opacity(pressed ? 1 : 0)
+                    
+                }
+                .font(.system(size: fontSize))
+                .foregroundColor(custom)
+                
+                
+                
+            }
+            .padding(20)
+            .foregroundColor(DarkMode())
+            .datePickerStyle(.graphical)
         }
-        .padding()
     }
+    func DarkMode() -> Color {
+        if mode == true {
+            return .black
+        }
+        else {
+            return .white
+        }
+    }
+    func BackgroundMode() -> Color {
+        if mode == true {
+            return .white
+        }
+        else {
+            return .black
+        }
+    }
+
 }
+
+
 
 #Preview {
     ContentView()
