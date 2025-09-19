1f39dfbf93d3c389c1e40411bd762087094f7066
BlakeT <BlakeTswift@gmail.com>
2025-09-19 13:39:27 -0600

    Initial Commit

---
+++ Birthday_Challenge/Birthday_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_ChallengeApp.swift
+//  Birthday_Challenge
+//
+//  Created by Trytten, Blake - Student on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday_ChallengeTests/Birthday_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_ChallengeTests.swift
+//  Birthday_ChallengeTests
+//
+//  Created by Trytten, Blake - Student on 9/19/25.
+//
+
+import Testing
+@testable import Birthday_Challenge
+
+struct Birthday_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
